40a9ba2678d5268f018c38350ac4b7dc397989cd
Zach Benton <Zach@Zach.local>
2017-05-10 16:01:27 -0500

    init js dev branch

--- template/js/init.js
+++ template/js/init.js
@@ -1,29 +1,40 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav({
-      menuWidth: 300,
-      edge: 'left',
-      closeOnClick: true,
-      draggable: true
-    });
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav({
+            menuWidth: 300,
+            edge: 'left',
+            closeOnClick: true,
+            draggable: true
+        });
+        $('.parallax').parallax();
 
-    var options = [
-      {selector: '#box-1', offset: 20, callback: function(el) {
-        Materialize.fadeInImage($(el));
-      } },
-      {selector: '#box-2', offset: 60, callback: function(el) {
-        Materialize.fadeInImage($(el));
-      } },
-      {selector: '#box-3', offset: 100, callback: function(el) {
-        Materialize.fadeInImage($(el));
-      } },
-      {selector: '#box-4', offset: 140, callback: function(el) {
-        Materialize.fadeInImage($(el));
-      } },
-    ];
-    Materialize.scrollFire(options);
+        var options = [{
+            selector: '#box-1',
+            offset: 20,
+            callback: function(el) {
+                Materialize.fadeInImage($(el));
+            }
+        }, {
+            selector: '#box-2',
+            offset: 60,
+            callback: function(el) {
+                Materialize.fadeInImage($(el));
+            }
+        }, {
+            selector: '#box-3',
+            offset: 100,
+            callback: function(el) {
+                Materialize.fadeInImage($(el));
+            }
+        }, {
+            selector: '#box-4',
+            offset: 140,
+            callback: function(el) {
+                Materialize.fadeInImage($(el));
+            }
+        }, ];
+        Materialize.scrollFire(options);
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
